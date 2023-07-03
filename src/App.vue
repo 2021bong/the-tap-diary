e4468bf19cf01b7da413ef100e6c8c124732c8ca
Bong <2021bongwonny@gmail.com>
2023-07-04 01:02:36 +0900

    ➕feat : firebase 연결
    html title 수정

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,43 @@
-<script setup lang="ts">
+<script lang="ts">
 import Month from './components/Month.vue';
+
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  Firestore,
+} from 'firebase/firestore/lite';
+import dayjs from 'dayjs';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+const db = getFirestore(app);
+
+const getData = async (database: Firestore) => {
+  const thisMonthData = collection(database, '2023_07');
+  const monthDataDoc = await getDocs(thisMonthData);
+  const monthDatas = monthDataDoc.docs.map((doc) => doc.data());
+  return monthDatas;
+};
+
+const data = await getData(db);
+data.forEach((data) => {
+  console.log('data level:', data.level);
+  console.log('data date:', dayjs(data.date).format('YYYY-MM-DD hh:mm:ss'));
+  console.log('data reason:', data.reason);
+});
 </script>
 
 <template>
